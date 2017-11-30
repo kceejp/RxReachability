c5017614ed0924ec8c125c69618a2d8751f38b1f
Eric-Paul Lecluse <e@epologee.com>
2017-11-30 16:09:34 +0100

    Create Xcode project with shared scheme for building with Carthage

---
+++ RxReachability/RxReachability.h
@@ -0,0 +1,19 @@
+//
+//  RxReachability.h
+//  RxReachability
+//
+//  Created by Eric-Paul Lecluse on 30-11-17.
+//  Copyright © 2017 ivanbruel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RxReachability.
+FOUNDATION_EXPORT double RxReachabilityVersionNumber;
+
+//! Project version string for RxReachability.
+FOUNDATION_EXPORT const unsigned char RxReachabilityVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RxReachability/PublicHeader.h>
+
+
